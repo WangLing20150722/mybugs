98cfd98424c534f2faef66718919485d3cc1689a
lihui02 <lihui02@tcl.com>
2018-05-20 15:47:48 +0800

    format log

--- src/works/RefreshDetailsBetween.go
+++ src/works/RefreshDetailsBetween.go
@@ -1,40 +1,39 @@
 package works
 
 import (
-	"time"
-	"utils"
 	"log"
 	"mantis"
+	"time"
+	"utils"
 )
 
-func RefreshDetailsBetween(start,end time.Time) error {
+func RefreshDetailsBetween(start, end time.Time) error {
 	var err error
 
-	var startDay,endDay time.Time
-	startDay,err = utils.FormatTime2Day(start)
-	if(err != nil) {
+	var startDay, endDay time.Time
+	startDay, err = utils.FormatTime2Day(start)
+	if err != nil {
 		log.Print("RefreshDetailsBwtween formatTime failed:", err)
 		return err
 	}
-	endDay,err = utils.FormatTime2Day(end)
-	if(err != nil) {
+	endDay, err = utils.FormatTime2Day(end)
+	if err != nil {
 		log.Print("RefreshDetailsBwtween formatTime failed:", err)
 		return err
 	}
-	log.Printf("RefreshDetailsBwtween %s --> %s\n",startDay.Format("2006-01-02"),endDay.Format("2006-01-02"))
+	log.Printf("RefreshDetailsBwtween %s --> %s\n", startDay.Format("2006-01-02"), endDay.Format("2006-01-02"))
 
-
-	if(!mantis.CheckLogin(nil)) {
-		_,err = mantis.Login(utils.CONFIG.Username,utils.CONFIG.Password)
-		if(err != nil) {
-			log.Printf("RefreshDetailsBetween Login failed %v\n",err)
+	if !mantis.CheckLogin(nil) {
+		_, err = mantis.Login(utils.CONFIG.Username, utils.CONFIG.Password)
+		if err != nil {
+			log.Printf("RefreshDetailsBetween Login failed %v\n", err)
 			return err
 		}
 	}
 
 	var issues []*mantis.Issue
-	issues,err = mantis.EachList(startDay,endDay)
-	if(err != nil) {
+	issues, err = mantis.EachList(startDay, endDay)
+	if err != nil {
 		log.Print("RefreshDetailsBwtween EachList failed:", err)
 		return err
 	}
@@ -42,39 +41,32 @@ func RefreshDetailsBetween(start,end time.Time) error {
 	for _, issue := range issues {
 
 		var detail *mantis.IssueDetail
-		detail,err = mantis.GetDetail(issue.Id)
-		if(err != nil) {
-			if(DEBUG) {
-				log.Printf("Issue [%d]%s no details, do Refresh",issue.Id,issue.Updated)
-			}
+		detail, err = mantis.GetDetail(issue.Id)
+		if err != nil {
+			log.Printf("Issue [%d] Updated at %s no details, do Refresh", issue.Id, issue.Updated)
 			doRefreshDetail(issue.Id)
 		} else {
-			ftday,_ := utils.FormatTime2Day(detail.FetchTime)
-			if(DEBUG) {
-				log.Printf("Issue [%d] Updated at %s Refreshed at %s\n",issue.Id,issue.Updated,ftday.Format("2006-01-02"))
-			}
-			updateTm,_ := time.Parse("2006-01-02", issue.Updated)
+			ftday, _ := utils.FormatTime2Day(detail.FetchTime)
+			updateTm, _ := time.Parse("2006-01-02", issue.Updated)
 
-			if(!ftday.After(updateTm)) {
-				if(DEBUG) {
-					log.Printf("	Issue updated, do Refresh")
-				}
+			if !ftday.After(updateTm) {
+				log.Printf("Issue [%d] Updated at %s Refreshed at %s, do Refresh\n", issue.Id, issue.Updated, ftday.Format("2006-01-02"))
 				doRefreshDetail(issue.Id)
+			} else {
+				log.Printf("Issue [%d] Updated at %s Refreshed at %s, will not Refresh\n", issue.Id, issue.Updated, ftday.Format("2006-01-02"))
 			}
 		}
 	}
 
-
 	return nil
-
 
 }
 
 func doRefreshDetail(id int64) error {
-	detail,err := mantis.GetIssueDetail(id)
+	detail, err := mantis.GetIssueDetail(id)
 
-	if(err != nil) {
-		log.Printf("doRefreshDetail failed:%v\n",err)
+	if err != nil {
+		log.Printf("doRefreshDetail failed:%v\n", err)
 		return err
 	}
 	mantis.SaveDetail(detail)
