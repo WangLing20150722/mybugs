9aadd430040064742da5c92e803de75b0e53362b
lihui02 <lihui02@tcl.com>
2018-05-21 21:49:00 +0800

    remove cross compile and use native one. cgo is need by sqlite3 but CGO_ENABLED=1 may not work well with a cross compiling c compiler. ref: https://stackoverflow.com/questions/32309030/go-1-5-cross-compile-using-cgo-on-os-x-to-linux-and-windows

--- build.sh
+++ build.sh
@@ -1,6 +1,4 @@
 #!/usr/bin/env bash
 
 . ./env.sh
-GOOS=linux GOARCH=amd64 go build -o mybugs_linux src/main/main_goquery.go
-GOOS=windows GOARCH=amd64 go build -o mybugs_win.exe src/main/main_goquery.go
-GOOS=darwin GOARCH=amd64 go build -o mybugs_darwin src/main/main_goquery.go
+go build -o mybugs src/main/main_goquery.go
