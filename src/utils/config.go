e59fc7a82f88013c1a4d814ae8046574fa3aa505
lihui02 <lihui02@tcl.com>
2018-05-22 09:56:19 +0800

    only use password.json in dev

--- src/utils/config.go
+++ src/utils/config.go
@@ -16,19 +16,22 @@ var CONFIG CONFIG_S
 
 func init() {
 	//For testcase, save username & password in password.json
-	filename := os.Getenv("GOPATH") + "/password.json"
+	gopath := os.Getenv("GOPATH")
+	if gopath != "" {
+		filename := os.Getenv("GOPATH") + "/password.json"
 
-	bytes, err := ioutil.ReadFile(filename)
-	if err != nil {
-		log.Printf("ReadNameList ReadFile error:%s", err)
-		return
+		bytes, err := ioutil.ReadFile(filename)
+		if err != nil {
+			log.Printf("ReadNameList ReadFile error:%s", err)
+			return
+		}
+
+		err = json.Unmarshal(bytes, &CONFIG)
+		if err != nil {
+			log.Printf("ReadNameList Unmarshal error:%s", err)
+			return
+		}
+
+		log.Print("Read password from password.json for test:", CONFIG)
 	}
-
-	err = json.Unmarshal(bytes, &CONFIG)
-	if err != nil {
-		log.Printf("ReadNameList Unmarshal error:%s", err)
-		return
-	}
-
-	log.Print("Read password from password.json for test:", CONFIG)
 }
